0bdb59b5275edc34d3cd625ee59df7a2ef1b034d
Matheus Batista <matheus.batista@lightera.com>
2025-07-14 16:07:53 -0300

    refactor: create new func to walk through nodes

--- internal/core/engine.go
+++ internal/core/engine.go
@@ -73,48 +73,7 @@ func findRootCauses(node *data.Node) {
 func drawGraphs(node *data.Node) error {
 	g := graph.New(graph.IntHash, graph.Directed())
 
-	var walk func(n *data.Node) error
-	walk = func(n *data.Node) error {
-		var attr func(*graph.VertexProperties)
-		if node.RootCause {
-			attr = graph.VertexAttribute("color", "blue")
-		} else {
-			attr = graph.VertexAttribute("color", "black")
-		}
-
-		_ = g.AddVertex(n.ID, graph.VertexAttribute("label", n.Name), attr)
-
-		for _, child := range n.Children {
-			var attr func(*graph.VertexProperties)
-			if !child.Active {
-				attr = graph.VertexAttribute("color", "red")
-			} else {
-				attr = graph.VertexAttribute("color", "green")
-			}
-
-			if child.RootCause {
-				attr = graph.VertexAttribute("color", "blue")
-			}
-
-			err := g.AddVertex(child.ID, graph.VertexAttribute("label", child.Name), attr)
-			if err != nil {
-				return fmt.Errorf("add vertex. %w", err)
-			}
-			err = g.AddEdge(n.ID, child.ID)
-			if err != nil {
-				return fmt.Errorf("add edge. %w", err)
-			}
-
-			err = walk(child)
-			if err != nil {
-				return fmt.Errorf("walk. %w", err)
-			}
-		}
-
-		return nil
-	}
-
-	err := walk(node)
+	err := walk(g, node)
 	if err != nil {
 		return err
 	}
@@ -129,6 +88,46 @@ func drawGraphs(node *data.Node) error {
 	err = draw.DOT(g, file)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func walk(g graph.Graph[int, int], n *data.Node) error {
+	var attr func(*graph.VertexProperties)
+	if n.RootCause {
+		attr = graph.VertexAttribute("color", "blue")
+	} else {
+		attr = graph.VertexAttribute("color", "black")
+	}
+
+	_ = g.AddVertex(n.ID, graph.VertexAttribute("label", n.Name), attr)
+
+	for _, child := range n.Children {
+		var attr func(*graph.VertexProperties)
+		if !child.Active {
+			attr = graph.VertexAttribute("color", "red")
+		} else {
+			attr = graph.VertexAttribute("color", "green")
+		}
+
+		if child.RootCause {
+			attr = graph.VertexAttribute("color", "blue")
+		}
+
+		err := g.AddVertex(child.ID, graph.VertexAttribute("label", child.Name), attr)
+		if err != nil {
+			return fmt.Errorf("add vertex. %w", err)
+		}
+		err = g.AddEdge(n.ID, child.ID)
+		if err != nil {
+			return fmt.Errorf("add edge. %w", err)
+		}
+
+		err = walk(g, child)
+		if err != nil {
+			return fmt.Errorf("walk. %w", err)
+		}
 	}
 
 	return nil
