461df45814ca7ab85cfd40535d15847dcd9678b8
Matheus Batista <matheus.batista@lightera.com>
2025-07-29 11:22:44 -0300

    refactor: removed example from code

--- internal/api/server.go
+++ internal/api/server.go
@@ -11,8 +11,6 @@ import (
 func NewServer(logger *slog.Logger, models *data.Models, services *aws.Services) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.Handle("/example", HandleExample(logger, models, services))
-
 	mux.Handle("POST /correlation/{tenant_id}/{project_id}", HandleCorrelation(logger, models, services))
 
 	return mux
